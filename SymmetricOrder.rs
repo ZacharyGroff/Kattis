f6e6835d24f7194dcc5c0835336b6ed5f157e24d
Zachary Groff <groff.zackm@gmail.com>
2019-07-30 08:48:08 -0400

    need to fix sorting

---
+++ SymmetricOrder.rs
@@ -0,0 +1,64 @@
+fn main() {
+  let mut set: u32 = 1;
+  loop {
+    let len: u32 = get_set_length();
+    if len == 0 {
+      break;
+    }
+    let mut names: Vec<String> = get_set_names(len);
+    names = sort_names(names);
+    let (names1, names2) = split_names(names);
+    let names = merge_names(names1, names2);
+    print_set(names, set);
+    set += 1;
+  }
+}
+
+fn get_set_length() -> u32 {
+  let mut input = String::new();
+  std::io::stdin().read_line(&mut input).expect("read line error");
+  input.trim().parse().expect("wanted a number")
+}
+
+fn get_set_names(len: u32) -> Vec<String> {
+  let mut names = Vec::new();
+  for _ in 0..len {
+    let name: String = get_set_name();
+    names.push(name);
+  }
+
+  names
+}
+
+fn get_set_name() -> String {
+  let mut input = String::new();
+  std::io::stdin().read_line(&mut input).expect("read line error");
+  input.trim().to_string()
+}
+
+fn sort_names(mut names: Vec<String>) -> Vec<String> {
+  //sort name array by length of name
+  names.sort();
+  names
+}
+
+fn split_names(names: Vec<String>) -> (Vec<String>, Vec<String>){
+  //alternate pushing names into 2 different arrays
+  let len = names.len();
+  let half_len = len/2;
+  (names[0..half_len].to_vec(), names[half_len..len].to_vec())
+}
+
+fn merge_names(mut names1: Vec<String>, mut names2: Vec<String>) -> Vec<String> {
+  //reverse second array and merge together
+  names2.reverse();
+  names1.append(&mut names2);
+  names1
+}
+
+fn print_set(names: Vec<String>, set: u32) {
+  println!("SET {}", set);
+  for name in names {
+    println!("{}", name);
+  }
+}
