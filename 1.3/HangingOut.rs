60f0d780c00bc4b7308c4e1c0a5d6713cf33114d
Zachary Groff <groff.zackm@gmail.com>
2019-07-30 11:07:56 -0400

    fix split_names

--- 1.3/HangingOut.rs
+++ 1.3/HangingOut.rs
@@ -1,49 +1,73 @@
 fn main() {
-  let (limit, events) = get_initial_line();
-  let denied_groups = get_denied_groups(limit, events);
-  println!("{}", denied_groups);
+  let mut set: u32 = 1;
+  loop {
+    let len: u32 = get_set_length();
+    if len == 0 {
+      break;
+    }
+    let mut names: Vec<String> = get_set_names(len);
+    names = sort_names(names);
+    let (names1, names2) = split_names(names);
+    let names = merge_names(names1, names2);
+    print_set(names, set);
+    set += 1;
+  }
 }
 
-fn get_initial_line() -> (i32, i32){
+fn get_set_length() -> u32 {
   let mut input = String::new();
   std::io::stdin().read_line(&mut input).expect("read line error");
-  let vec = input.split_whitespace().map(|x| x.parse::<i32>().expect("parse error")).collect::<Vec<i32>>();
-  
-  (vec[0], vec[1])
+  input.trim().parse().expect("wanted a number")
 }
 
-fn get_denied_groups(limit: i32, events: i32) -> i32 {
-  let mut denied_groups = 0;
-  let mut current_people = 0;
-  for _ in 0..events {
-    let (action, people) = get_event_line();
-    check_update(action, &mut current_people, people, limit, &mut denied_groups);
+fn get_set_names(len: u32) -> Vec<String> {
+  let mut names = Vec::new();
+  for _ in 0..len {
+    let name: String = get_set_name();
+    names.push(name);
   }
 
-  denied_groups
+  names
 }
 
-fn check_update(action: String, current_people: &mut i32, people: i32, limit: i32, denied_groups: &mut i32) {
-    if action == "leave" {
-      *current_people -= people;
-    } else if is_denied(limit, *current_people, people) {
-      *denied_groups += 1;        
-    } else {
-      *current_people += people;
-    }
-}
-
-fn is_denied(limit: i32, current_people: i32, additional: i32) -> bool {
-  limit < current_people + additional
-}
-
-fn get_event_line() -> (String, i32){
+fn get_set_name() -> String {
   let mut input = String::new();
   std::io::stdin().read_line(&mut input).expect("read line error");
-  let vec: Vec<String> = input.split_whitespace().map(|x| x.to_string()).collect();
-  
-  let action: String = vec[0].to_string();
-  let people: i32 = vec[1].parse::<i32>().unwrap();
+  input.trim().to_string()
+}
 
-  (action, people)
+fn sort_names(mut names: Vec<String>) -> Vec<String> {
+  //sort name array by length of name
+  names.sort_by(|a, b| a.len().cmp(&b.len()));
+  names
+}
+
+fn split_names(names: Vec<String>) -> (Vec<String>, Vec<String>){
+  //alternate pushing names into 2 different arrays
+  let mut names1 = Vec::new();
+  let mut names2 = Vec::new();
+
+  for i in 0..names.len() {
+    if i % 2 == 0 {
+      names1.push(names[i].clone());
+    } else {
+      names2.push(names[i].clone());
+    }
+  }
+
+  (names1, names2)
+}
+
+fn merge_names(mut names1: Vec<String>, mut names2: Vec<String>) -> Vec<String> {
+  //reverse second array and merge together
+  names2.reverse();
+  names1.append(&mut names2);
+  names1
+}
+
+fn print_set(names: Vec<String>, set: u32) {
+  println!("SET {}", set);
+  for name in names {
+    println!("{}", name);
+  }
 }
