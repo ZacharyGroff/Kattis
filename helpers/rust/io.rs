50a915dfed3c153093d7980be4e17f969c862620
Zachary Groff <groff.zackm@gmail.com>
2019-08-06 12:50:55 -0400

    get rid of warning

--- helpers/rust/io.rs
+++ helpers/rust/io.rs
@@ -1,7 +1,8 @@
 mod io {
 	fn get_input_line<T: std::str::FromStr>() -> Result<Vec<T>, <T as std::str::FromStr>::Err> {
 		let mut input = String::new();
-		std::io::stdin().read_line(&mut input);
+		let _ = std::io::stdin().read_line(&mut input);
+		
 		input.split_whitespace().map(|x| x.parse::<T>()).collect()
 	}
 }
