15e4bf24a894ac0a55d64472058c0308090f1e3c
Zachary Groff <groff.zackm@gmail.com>
2019-08-07 13:35:31 -0400

    cleanup HardwoodSpecies

--- 2.4/HardwoodSpecies.rs
+++ 2.4/HardwoodSpecies.rs
@@ -1,6 +1,17 @@
 fn main() {
+  let tree_map = get_trees();
+  
+  let mut tree_vec: Vec<(&String, &u64)> = tree_map
+    .iter()
+    .collect();
+  tree_vec.sort_by(|a, b| a.0.cmp(b.0));
+
+  let total_trees: u64 = sum_trees(&tree_vec);
+  print_trees(&tree_vec, total_trees);  
+}
+
+fn get_trees() -> std::collections::HashMap<String, u64> {
   let mut map = std::collections::HashMap::new();
-  let mut total_trees = 0;
   loop {
     let tree = try_get_input_line();
     if tree == "" {
@@ -8,14 +19,9 @@ fn main() {
     }
     let count = map.entry(tree).or_insert(0);
     *count += 1;
-    total_trees += 1;
   }
-  let mut count_vec: Vec<(&String, &u64)> = map.iter().collect();
-  count_vec.sort_by(|a, b| a.0.cmp(b.0));
-  for (species, count) in count_vec {
-    let formatted_count = format_count(*count as f64, total_trees as f64);
-    println!("{} {}", *species, formatted_count);
-  }
+
+  map
 }
 
 fn try_get_input_line() -> String {
@@ -28,4 +34,18 @@ fn try_get_input_line() -> String {
 
 fn format_count(count: f64, total_trees: f64) -> f64 {
   ((count / total_trees) * 100000000.).round() / 1000000.
+}
+
+fn sum_trees(tree_vec: &Vec<(&String, &u64)>) -> u64 {
+  tree_vec
+    .iter()
+    .map(|x| x.1)
+    .sum()
+}
+
+fn print_trees(tree_vec: &Vec<(&String, &u64)>, total_trees: u64) {
+  for (species, count) in tree_vec {
+    let formatted_count = format_count(**count as f64, total_trees as f64);
+    println!("{} {}", *species, formatted_count);
+  }
 }
