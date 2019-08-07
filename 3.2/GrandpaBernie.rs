58d068f994998b265385d980d5da09e1e953ba3f
Zachary Groff <groff.zackm@gmail.com>
2019-08-07 08:51:34 -0400

    passing all tests

--- 3.2/GrandpaBernie.rs
+++ 3.2/GrandpaBernie.rs
@@ -1,14 +1,16 @@
  use std::collections::HashMap;
  
  fn main() {
-  let n = get_input_line::<u16>().unwrap()[0];
-  let mut trips: HashMap<String, Vec<u16>> = HashMap::new();
+  let n = get_input_line::<u64>().unwrap()[0];
+  let mut trips: HashMap<String, Vec<u64>> = HashMap::new();
   for _ in 0..n {
     let line = get_input_line::<String>().unwrap();
     update_trips(line, &mut trips)
   }
+
+  sort_trips(&mut trips);
   
-  let queries = get_input_line::<u16>().unwrap()[0];
+  let queries = get_input_line::<u64>().unwrap()[0];
   for _ in 0..queries {
     let line = get_input_line::<String>().unwrap();
     let year = query(line, &mut trips);
@@ -23,19 +25,23 @@ fn get_input_line<T: std::str::FromStr>() -> Result<Vec<T>, <T as std::str::From
 		input.split_whitespace().map(|x| x.parse::<T>()).collect()
 }
 
-fn update_trips(line: Vec<String>, trips: &mut HashMap<String, Vec<u16>>) {
+fn update_trips(line: Vec<String>, trips: &mut HashMap<String, Vec<u64>>) {
   let destination = line[0].clone();
-  let year: u16 = line[1].parse().unwrap();
+  let year: u64 = line[1].parse().unwrap();
   let vec = trips.entry(destination).or_insert(Vec::new());
   vec.push(year);
 }
 
-fn query(line: Vec<String>, trips: &mut HashMap<String, Vec<u16>>) -> u16 {
+fn query(line: Vec<String>, trips: &mut HashMap<String, Vec<u64>>) -> u64 {
   let destination = line[0].clone();
   let nth_trip: usize = line[1].parse().unwrap();
 
   let vec = trips.entry(destination).or_default();
-  vec.sort();
-	
   vec[nth_trip - 1].clone()
+}
+
+fn sort_trips(trips: &mut HashMap<String, Vec<u64>>) {
+  for (_, vec) in trips {
+    vec.sort();
+  }
 }
