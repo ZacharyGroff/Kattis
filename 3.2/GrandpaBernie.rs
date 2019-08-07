e0ca66ebeab66845c98a036e0b864f4b7b1d146b
Zachary Groff <groff.zackm@gmail.com>
2019-08-07 08:43:16 -0400

    runtime error

--- 3.2/GrandpaBernie.rs
+++ 3.2/GrandpaBernie.rs
@@ -1,17 +1,17 @@
  use std::collections::HashMap;
- use std::collections::BinaryHeap;
  
  fn main() {
   let n = get_input_line::<u16>().unwrap()[0];
-  let mut trips: HashMap<String, BinaryHeap<u16>> = HashMap::new();
+  let mut trips: HashMap<String, Vec<u16>> = HashMap::new();
   for _ in 0..n {
     let line = get_input_line::<String>().unwrap();
     update_trips(line, &mut trips)
   }
+  
   let queries = get_input_line::<u16>().unwrap()[0];
   for _ in 0..queries {
     let line = get_input_line::<String>().unwrap();
-    let year = query(line, &trips);
+    let year = query(line, &mut trips);
     println!("{}", year);
   }
  }
@@ -23,22 +23,19 @@ fn get_input_line<T: std::str::FromStr>() -> Result<Vec<T>, <T as std::str::From
 		input.split_whitespace().map(|x| x.parse::<T>()).collect()
 }
 
-fn update_trips(line: Vec<String>, trips: &mut HashMap<String, BinaryHeap<u16>>) {
+fn update_trips(line: Vec<String>, trips: &mut HashMap<String, Vec<u16>>) {
   let destination = line[0].clone();
   let year: u16 = line[1].parse().unwrap();
-  let heap = trips.entry(destination).or_insert(BinaryHeap::new());
-  heap.push(year);
+  let vec = trips.entry(destination).or_insert(Vec::new());
+  vec.push(year);
 }
 
-fn query(line: Vec<String>, trips: &HashMap<String, BinaryHeap<u16>>) -> u16{
+fn query(line: Vec<String>, trips: &mut HashMap<String, Vec<u16>>) -> u16 {
   let destination = line[0].clone();
-  let nth_trip: u8 = line[1].parse().unwrap();
+  let nth_trip: usize = line[1].parse().unwrap();
 
-  let mut year: u16 = 0;
-  for _ in 0..nth_trip {
-    let heap = trips.entry(destination).or_default();
-    year = heap.pop();
-  }
-
-  year
+  let vec = trips.entry(destination).or_default();
+  vec.sort();
+	
+  vec[nth_trip - 1].clone()
 }
