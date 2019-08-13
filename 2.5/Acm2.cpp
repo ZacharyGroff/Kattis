0446848775daab4715141c68a472e50e6bc7ddcf
Zachary Groff <groff.zackm@gmail.com>
2019-08-13 11:47:10 -0400

    cleanup acm2

--- 2.5/Acm2.cpp
+++ 2.5/Acm2.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <algorithm>
 
+void solve_problem(int problem, int* minutes_remaining, int* time_penalty, int* problems_solved) {
+      *minutes_remaining -= problem;
+      *time_penalty += 300 - *minutes_remaining;
+      (*problems_solved)++;
+}
+
 int main() {
   int num_problems, first_index;
   std::cin >> num_problems >> first_index;
@@ -13,22 +19,20 @@ int main() {
   int minutes_remaining = 300;
   int time_penalty = 0;
   int problems_solved = 0;
+
   if (problems[first_index] > minutes_remaining) {
-    std::cout << "0 0" << std::endl;
+    std::cout << problems_solved << " " << time_penalty << std::endl;
     return 0;
   }
-  minutes_remaining -= problems[first_index];
+
+  solve_problem(problems[first_index], &minutes_remaining, &time_penalty, &problems_solved);
   problems[first_index] = -1;
-  time_penalty += 300 - minutes_remaining;
-  problems_solved++;
 
   std::sort(problems, problems+num_problems);
 
   for (int i = 1; i < num_problems; i++) {
     if (minutes_remaining >= problems[i]) {
-      minutes_remaining -= problems[i];
-      time_penalty += 300 - minutes_remaining;
-      problems_solved++;
+      solve_problem(problems[i], &minutes_remaining, &time_penalty, &problems_solved);
     }
   }
 
