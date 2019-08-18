b5c0e33937de38d945e68f94e4f8fe504035796e
Zachary Groff <groff.zackm@gmail.com>
2019-08-18 14:00:28 -0400

    reformat VirtualFriends

--- 4.8/VirtualFriends.cpp
+++ 4.8/VirtualFriends.cpp
@@ -3,59 +3,63 @@
 #include <map>
 #include <string.h>
 
+class UF {
+  int *id, cnt, *sz;
 
-class UF    {
-    int *id, cnt, *sz;
-public:
-    // Create an empty union find data structure with N isolated sets.
-    UF(int N)   {
-        cnt = N;
-    id = new int[N];
-    sz = new int[N];
-        for(int i=0; i<N; i++)  {
-            id[i] = i;
+  public:
+    UF(int N) {
+      cnt = N;
+      id = new int[N];
+      sz = new int[N];
+      for(int i=0; i<N; i++)  {
+        id[i] = i;
         sz[i] = 1;
+      }
     }
+
+    ~UF() {
+      delete [] id;
+      delete [] sz;
     }
-    ~UF()   {
-    delete [] id;
-    delete [] sz;
-    }
-    // Return the id of component corresponding to object p.
+
     int find(int p) {
-        int root = p;
-        while (root != id[root])
-            root = id[root];
-        while (p != root) {
-            int newp = id[p];
-            id[p] = root;
-            p = newp;
-        }
-        return root;
+      int root = p;
+      while (root != id[root]) {
+          root = id[root];
+      }
+      while (p != root) {
+        int newp = id[p];
+        id[p] = root;
+        p = newp;
+      }
+
+      return root;
     }
-    // Replace sets containing x and y with their union.
-    void merge(int x, int y)    {
-        int i = find(x);
-        int j = find(y);
-        if (i == j) return;
-        
-        // make smaller root point to larger one
-        if   (sz[i] < sz[j])    { 
+    
+    void merge(int x, int y) {
+      int i = find(x);
+      int j = find(y);
+      if (i == j) {
+        return;
+      }
+
+      if (sz[i] < sz[j]) { 
         id[i] = j; 
         sz[j] += sz[i]; 
-    } else  { 
+      } else { 
         id[j] = i; 
         sz[i] += sz[j]; 
+      }
+
+      cnt--;
     }
-        cnt--;
+
+    bool connected(int x, int y) {
+      return find(x) == find(y);
     }
-    // Are objects x and y in the same set?
-    bool connected(int x, int y)    {
-        return find(x) == find(y);
-    }
-    // Return the number of disjoint sets.
+
     int count() {
-        return cnt;
+      return cnt;
     }
 
     int size(int n) {
@@ -79,14 +83,14 @@ int main() {
       std::getline(std::cin, line);
       std::string f1 = line.substr(0, line.find(" "));
       std::string f2 = line.substr(line.find(" ")+1, line.length());
-      
+
       if (map.find(f1) == map.end()) {
         map[f1] = unique;
-    unique++;
+        unique++;
       }
       if (map.find(f2) == map.end()) {
-    map[f2] = unique;
-    unique++;
+        map[f2] = unique;
+        unique++;
       }
       ds.merge(map[f1], map[f2]);
       std::cout << ds.size(ds.find(map[f1])) << "\n";
