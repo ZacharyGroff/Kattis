b6b9b0e20e5b9284642a207f2ccecaa1701dbc42
Zachary Groff <groff.zackm@gmail.com>
2019-08-20 13:16:36 -0400

    Create Classy.py

---
+++ 4.3/Classy.py
@@ -0,0 +1,37 @@
+def get_pair():
+  inp = input().split()
+  name = inp[0][:-1]
+  rank = inp[1].split('-')[::-1]
+  rank = ''.join([k[0] for k in rank])
+  return [name, rank]
+
+def get_pairs():
+  pairs = []
+  num_pairs = int(input())
+  for _ in range(num_pairs):
+    pair = get_pair()
+    pairs.append(pair)
+  return pairs
+
+def sort_pairs(pairs):
+  max_len = len(max(pairs, key = lambda x: len(x[1]))[1])
+  for pair in pairs:
+    pair[1] = pair[1] + ('m' * (max_len - len(pair[1])))
+  pairs.sort(key=lambda y: y[0])
+  pairs.sort(key=lambda y: y[1], reverse=True)
+  return pairs
+
+def print_test_case(pairs):
+    for pair in pairs:
+        print(pair[0])
+    print('='*30)
+
+def main():
+  num_tests = int(input())
+  for _ in range(num_tests):
+    pairs = get_pairs()
+    pairs = sort_pairs(pairs)
+    print_test_case(pairs)
+
+if __name__ == '__main__':
+  main()
